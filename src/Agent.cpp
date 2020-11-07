87402f5de48ed27dd6102dccfc39064f7251160e
rs <ran.sayada@gmail.com>
2020-11-07 12:12:14 +0200

    constractors on agent class

--- src/Agent.cpp
+++ src/Agent.cpp
@@ -1,4 +1,23 @@
-//
-// Created by spl211 on 04/11/2020.
-//
+#include <iostream>
+#include "../include/Agent.h"
+//TODO: check about this
+using namespace std;
 
+Agent::Agent(Session &session, Session &session1) : session(session1) {}
+
+
+void Agent::act() {
+
+}
+ContactTracer::ContactTracer( Session &session) : Agent(session) {
+
+}
+void ContactTracer::act() {
+
+}
+
+Virus::Virus(int nodeInd, Session &session)  {}
+
+void Virus::act() {
+
+}
